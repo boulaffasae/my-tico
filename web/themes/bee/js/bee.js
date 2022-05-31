3ed809c9af6609e8a939b4093ae2745646601396
abir lamlih <a.lamlih@fullwaveagency.com>
2022-05-31 14:36:31 +0100

    feature/bee theme bee

---
+++ web/themes/bee/js/bee.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * bee behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.bee = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
