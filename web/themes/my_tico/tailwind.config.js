b92f3bd3e71295d99daeab8e908be3cb71271e7b
boulaffasae <boulaffasae@gmail.com>
2022-06-13 10:32:40 +0100

    Theming Home

---
+++ web/themes/my_tico/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./templates/**/*.html.twig"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#61CE70",
+        divider: "#C9E2F8",
+        primary: "#F37335",
+        secondary: "#135E9E",
+      },
+    },
+  },
+  plugins: [],
+};
