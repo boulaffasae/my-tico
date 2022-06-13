b06226b972c1ad4fb91d3a1b477dd0d104402ac4
boulaffasae <boulaffasae@gmail.com>
2022-06-13 08:40:01 +0100

    Create new theme

---
+++ web/themes/my_tico/js/my-tico.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * My Tico behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.myTico = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
